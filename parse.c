452a817f91003b02621038d69f47c094d37486b8
Ahish9009 <ahishjayant9009@gmail.com>
2020-07-05 02:37:45 +0400

    parsing: buggy

--- parse.c
+++ parse.c
@@ -5,18 +5,6 @@
 
 #include "utils.h"
 
-/*Commands_s commands;*/
-
-void strip(char *x) {
-
-	while (*x == ' ') x++;
-	int len = strlen(x);
-	while (len > 0 && x[len]==' ') len--;
-	x[len]=0;
-
-	printf("len: %d\n", len);
-}
-
 Commands_s * get_commands(Commands_s *commands, char *inp) {
 
 	bool in_sq=0, in_dq=0, esc=0;
@@ -28,48 +16,54 @@ Commands_s * get_commands(Commands_s *commands, char *inp) {
 		if (inp[i] == '\"') in_dq = !in_dq;
 		if (inp[i] == ';' && !(esc | in_dq | in_sq)) {
 
-			Cmd_s *new_cmd = (Cmd_s*) malloc(sizeof(Cmd_s));
+			Cmd_s *new_cmd = (Cmd_s*) malloc (sizeof(Cmd_s));
 			new_cmd->full_cmd = (char *) malloc (MAX_INPUT_SIZE*sizeof(char));
 
 			strcpy(new_cmd->full_cmd, inp);
 			new_cmd->full_cmd[i]=0; new_cmd->full_cmd+=s;
-			strip(new_cmd->full_cmd);
+			new_cmd->full_cmd = strip(new_cmd->full_cmd);
 
-			commands->cmd_lst[commands->cnt++] = new_cmd;
+			if (!is_empty(new_cmd->full_cmd)) {
+				commands->cmd_lst[commands->cnt++] = new_cmd;
+				fprintf(stderr, "here\n");
+			}
 
 			s=i+1;
 		}
-
-		// handle escaping
-		esc = 0;
-		if (inp[i] == '\\') {
-			if (esc) esc = 0;
-			else esc = 1;
-		}
-		
+		if (inp[i] == '\\') esc = !esc;
 	}
-	for (int i = 0; i < commands->cnt; i++) 
-		printf("%s\n", commands->cmd_lst[i]->full_cmd);
+	/*if (DEBUG) {*/
+		for (int i = 0; i < commands->cnt; i++) 
+			fprintf(stdout, "%s\n", commands->cmd_lst[i]->full_cmd);
+	/*}*/
 
 	return commands;
 }
 
 Commands_s * parse(char *inp) {
 	
+	char inp2[strlen(inp)+2];
+	strcpy(inp2, inp);
+	inp2[strlen(inp)] = ';';
+	inp2[strlen(inp)+1] = 0;
+	
+
 	Commands_s *commands = (Commands_s*) malloc(sizeof(Commands_s));
 	commands->cmd_lst = (Cmd_s**) malloc (MAX_BUFFER_CMDS*sizeof(Cmd_s*));
-	commands = get_commands(commands, inp);
+	commands = get_commands(commands, inp2);
 
 	return commands;
 }
 
-int main() {
+/*int main() {*/
 	
-	char inp[] = "Hello worl \"d;c\'lolzor;\'o; \" mment allez vous?;     je vais bien  ; lolzor ;";
-	parse(inp);
+	/*char inp[] = "echo \"hello world\" ; ls  ; ls -l; ls -aR ";*/
+	/*char inp2[strlen(inp)+2];*/
+	/*strcpy(inp2, inp);*/
+	/*inp2[strlen(inp)] = ';';*/
+	/*inp2[strlen(inp)+1] = 0;*/
+	/*parse(inp2);*/
 
-	printf("\n");
+	/*return 0;*/
 
-	return 0;
-
-}
+/*}*/
---
+++ parse.h
@@ -0,0 +1,8 @@
+#include"utils.h"
+
+#ifndef _PARSE__1
+#define _PARSE__1
+
+Commands_s * parse(char *inp);
+
+#endif
