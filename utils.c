f43e0005266088f624260587749479bdeb6d09bc
Ahish9009 <ahishjayant9009@gmail.com>
2020-07-05 02:36:58 +0400

    prompt added, edgy is_empty

--- utils.c
+++ utils.c
@@ -1,5 +1,61 @@
 #include<stdlib.h>
 #include<ctype.h>
+#include<string.h>
+#include<stdbool.h>
+#include<unistd.h>
+#include<stdio.h>
+
+#include"prompt.h"
+#include"utils.h"
+#include"parse.h"
+
+pid_t shell_pid;
+char *user;
+char *home_path;
+char *path;
 
 
+char * strip(char *x) {
 
+	while (*x == ' ') x++;
+
+	int len = strlen(x)-1;
+	while (len > 0 && x[len]==' ') len--;
+	x[len+1]=0;
+
+	return x;
+}
+
+bool is_empty(char *x) {
+	if (!x) return 1;
+	fprintf(stderr, "%lu\n", strlen(x));
+	if (!strlen(x)) return 1;
+	for (int i = 0; i < strlen(x); i++) {
+		fprintf(stderr, "%c\n", x[i]);
+		if (x[i] != ' ') return 0;
+	}
+	return 1;
+}
+
+void init() {
+
+	home_path = getenv("PWD");
+	user = getenv("USER");
+	shell_pid = getpid();
+	path = "~";
+	
+}
+
+void repl() {
+
+	while(1) {
+		char *prompt = get_prompt();
+		fprintf(stdout, "%s", prompt);
+
+		char *inp = (char *) malloc(MAX_INPUT_SIZE*sizeof(char));
+		fgets(inp, MAX_INPUT_SIZE, stdin);
+
+		parse(inp);
+	}
+
+}
--- utils.h
+++ utils.h
@@ -1,8 +1,11 @@
 #include<stdlib.h>
+#include<stdbool.h>
 #include<ctype.h>
 
 #ifndef __UTILS_1
 #define __UTILS_1
+
+#define DEBUG 1
 
 #define RED     "\x1b[31m"
 #define GREEN   "\x1b[32m"
@@ -13,6 +16,7 @@
 #define MAX_INPUT_SIZE  100
 #define MAX_BUFFER_CMDS 20
 #define MAX_TOKENS      100
+#define MAX_PROMPT_LEN  150
 
 typedef struct {
 	char *full_cmd;
@@ -24,5 +28,14 @@ typedef struct {
 	Cmd_s **cmd_lst;
 		
 } Commands_s;
+
+char *user;
+char *home_path;
+char *path;
+
+void init();
+void repl();
+char * strip(char *x);
+bool is_empty(char *x);
 
 # endif
