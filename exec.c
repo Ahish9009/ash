e392163158e38cb4c14e3cfc5ef402b4cec27119
Ahish9009 <ahishjayant9009@gmail.com>
2020-07-07 15:46:44 +0400

    basic execution

---
+++ exec.c
@@ -0,0 +1,32 @@
+#include<stdlib.h>
+#include<ctype.h>
+#include<stdio.h>
+#include<unistd.h>
+
+int launch_process(char **args) {
+	
+	pid_t pid, wpid;
+	int status;
+
+	pid = fork();
+	//child process
+	if (pid == 0) {
+
+		fprintf(stderr, "%s\n", args[0]);
+		if (execvp(args[0], args) == -1) {
+			perror("ash");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0) {
+		perror("ash");
+	}
+	else {
+		do {
+		wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	return 1;
+}
+
---
+++ exec.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#ifndef __EXEC_1
+#define __EXEC_1
+
+int launch_process(char **args);
+
+
+#endif
