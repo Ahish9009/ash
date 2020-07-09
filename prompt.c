136b57b3c0f26dea3abaacadc0aa159224e525ad
Ahish9009 <ahishjayant9009@gmail.com>
2020-07-10 02:48:01 +0400

    added bg proc running display, hostname

--- prompt.c
+++ prompt.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include"utils.h"
+#include"processes.h"
 
 char * get_prompt() {
 	
 	char * prompt = (char *) malloc (MAX_PROMPT_LEN*sizeof(char));
+	char *bg_proc_symb = (any_bg_process()) ? "*" : "";
 
 	char *prefix = "[as̶h]";
-		sprintf(prompt, YELLOW "%s" CLR_RST " %s@%s" BLUE ":%s" YELLOW " $ " CLR_RST, prefix, user, user, path);
+		sprintf(prompt, YELLOW "%s%s" CLR_RST " %s@%s" BLUE ":%s" YELLOW " $ " CLR_RST, prefix, bg_proc_symb, user, hostname, path);
 
 	return prompt;
 }
