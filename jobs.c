bad8d09dbe04904957ffb3b18c9435f88bf9e8dc
Ahish9009 <ahishjayant9009@gmail.com>
2020-07-10 20:50:47 +0400

    don't remember what I changed

--- jobs.c
+++ jobs.c
@@ -3,9 +3,9 @@
 #include"utils.h"
 #include"processes.h"
 
-void jobs(Cmd_s cmd) {
+void jobs(Cmd_s *cmd) {
 
-	if (cmd.argc > 1) {
+	if (cmd->argc > 1) {
 		fprintf(stderr, "usage: jobs\n");
 		return;
 	}
