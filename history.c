19be86c8cb60b8a6f8ce4bcab5ef25fe07e58733
Ahish9009 <ahishjayant9009@gmail.com>
2020-07-10 14:22:37 +0400

    history works

---
+++ history.c
@@ -0,0 +1,25 @@
+#include<string.h>
+#include<stdio.h>
+
+#include"utils.h"
+
+void insert_hist(char *inp) {
+
+	if (MAX_HIST_SIZE == 0) return;
+
+	if (hist.n == MAX_HIST_SIZE && MAX_HIST_SIZE > 1) {
+		for (int i = 1; i < hist.n; i++) {
+			strcpy(hist.hist_arr[i-1], hist.hist_arr[i]);
+		}
+	}
+	else {
+		hist.n++;
+	}
+	strcpy(hist.hist_arr[hist.n - 1], inp);
+}
+void display_hist() {
+
+	for (int i = 0; i < hist.n; i++) {
+		fprintf(stdout, "%s\n", hist.hist_arr[i]);
+	}
+}
