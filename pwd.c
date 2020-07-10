4aab8249aafa5500ad712d6ad95301381cba0e5b
Ahish9009 <ahishjayant9009@gmail.com>
2020-07-11 03:53:17 +0400

    minor changes

--- pwd.c
+++ pwd.c
@@ -3,7 +3,18 @@
 
 #include"utils.h"
 
+bool validate_pwd(Cmd_s *cmd) {
+
+	if (cmd->argc > 1) {
+		fprintf(stderr, "Usage: pwd\n");
+		return 1;
+	}
+	return 0;
+}
+
 void pwd(Cmd_s *cmd) {
+
+	if (validate_pwd(cmd)) return;
 
 	char *buf = (char *) malloc (MAX_INPUT_SIZE*sizeof(char));
 	if (!getcwd(buf, MAX_INPUT_SIZE)) {
