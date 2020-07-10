3bd43729d546cea802564793bf029abad7c34c89
Ahish9009 <ahishjayant9009@gmail.com>
2020-07-10 20:47:37 +0400

    added echo

---
+++ echo.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+
+#include"utils.h"
+
+bool validate_echo(Cmd_s *cmd) {
+	return 0;
+}
+
+void echo(Cmd_s *cmd) {
+
+	if (validate_echo(cmd)) {
+		fprintf(stderr, "Usage: echo [string ...]\n");
+		return;
+	}
+	
+	bool no_nl = 0;
+	for (int i = 1; i < cmd->argc; i++) {
+		if (!strcmp(cmd->argv[i], "-n")) no_nl = 1;
+		fprintf(stdout, "%s ", cmd->argv[i]);
+	}
+	if (!no_nl) fprintf(stdout, "\n");
+
+}
