dfbc3454bfc22e0e78f5a9399407b6f7c83b7095
Ahish9009 <ahishjayant9009@gmail.com>
2020-07-12 22:58:42 +0530

    added lil more info

--- src-linux/pinfo.c
+++ src-linux/pinfo.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<unistd.h>
 #include<ctype.h>
 
 #include"utils.h"
@@ -21,8 +22,15 @@ void pinfo(Cmd_s *cmd) {
 	if (cmd->argc == 1) pid = shell_pid;
 	else pid = atoi(cmd->argv[1]);
 
+	char *exe_path = (char *) malloc (MAX_INPUT_SIZE*sizeof(char));
+	sprintf(exe_path, "/proc/%d/exe", pid);
+
+	char *exe = (char *) malloc (MAX_INPUT_SIZE*sizeof(char));
+	int l = readlink(exe_path, exe, MAX_INPUT_SIZE);
+	exe[l] = 0;
 
 	fprintf(stdout, "pid: %d\n", pid);
+	fprintf(stdout, "executable: %s\n", exe);
 
 
 }
