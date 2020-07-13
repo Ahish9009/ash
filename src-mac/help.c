393392f945edf8144fb96940244e513fb7c7892b
Ahish9009 <ahishjayant9009@gmail.com>
2020-07-14 04:20:14 +0530

    [mac] updated help

--- src-mac/help.c
+++ src-mac/help.c
@@ -11,15 +11,15 @@ void help(Cmd_s *cmd) {
 	}
 
 	fprintf(stdout, GREEN "Supported built-in commands: \
-			\n1. ls -[al] \
-			\n2. echo -[n] [string...] \
-			\n3. pwd \
-			\n4. fg [<pid>] \
-			\n5. bg [<pid>] \
-			\n6. history \
-			\n7. jobs \
-			\n8. kjob \
-			\n9. overkill \
+			\n 1. ls -[al] \
+			\n 2. echo -[n] [string...] \
+			\n 3. pwd \
+			\n 4. fg [<job id>] \
+			\n 5. bg [<job id>] \
+			\n 6. history \
+			\n 7. jobs \
+			\n 8. kjob \
+			\n 9. overkill \
 			\n10. cd [dir] \
 			\n11. pinfo [<pid>]\
 			\n12. cronjob -c [cmd] -t [time] -p [period]\
