4ca0209d7805a5f51771acf7cae569099196271d
Ahish9009 <ahishjayant9009@gmail.com>
2020-07-14 18:19:38 +0400

    updated linux history

--- src-linux/history.c
+++ src-linux/history.c
@@ -3,6 +3,14 @@
 
 #include"utils.h"
 
+bool validate_hist(Cmd_s *cmd) {
+	if (cmd->argc > 2) {
+		fprintf(stderr, "Usage: history <no. of commands>\n");
+		return 1;
+	}
+	return 0;
+}
+
 void insert_hist(char *inp) {
 
 	if (MAX_HIST_SIZE == 0) return;
@@ -18,7 +26,17 @@ void insert_hist(char *inp) {
 	strcpy(hist.hist_arr[hist.n - 1], inp);
 }
 void display_hist(Cmd_s *cmd) {
-	for (int i = 0; i < hist.n; i++) {
+	
+	if (validate_hist(cmd)) return;
+
+	int i = 0;
+	if (cmd->argc == 2) {
+		int n = atoi(cmd->argv[1]);
+		i = (hist.n - n >= 0) ? hist.n - n : 0;
+	}
+
+	while (i < hist.n) {
 		fprintf(stdout, "%d %s\n", i, hist.hist_arr[i]);
+		i++;
 	}
 }
